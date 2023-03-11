637b70efe7395fc237b0edb31a6fdab97a8e6f13
Orelso <orelso25@gmail.com>
2023-03-11 20:58:07 +0200

    fix warnings

--- src/App.js
+++ src/App.js
@@ -1,70 +1,58 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import './App.css';
-import SearchIcon from './search.svg';
-import MovieCard from './MovieCard';
+import React from "react";
+import { useState, useEffect } from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
 
 // 9adab058
 
-const API_URL = 'http://www.omdbapi.com?apikey=9adab058';
-
-
-const movie1 = {
-    Poster: "N/A",
-    Title: "Amazing Spiderman Syndrome",
-    Type: "movie",
-    Year: "2012",
-    imdbID: "tt2586634"
-}
-
+const API_URL = "http://www.omdbapi.com?apikey=9adab058";
 
 const App = () => {
-    const [movies, setMovies] = useState([]);
-    const [searchTerm, setSearchTerm] = useState('');
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
 
-    const searchMovies = async (title) => {
-        const response = await fetch(`${API_URL}&s=${title}`)
-        const data = await response.json();
+    setMovies(data.Search);
+    console.log(data.Search);
+  };
 
-        setMovies(data.Search)
-        console.log(data.Search);
-    }
+  useEffect(() => {
+    searchMovies("Pokemon");
+  }, []);
 
-    useEffect(() => {
-        searchMovies('Pokemon');
-    }, []);
+  return (
+    <div className="app">
+      <h1>Cinematic Adventure</h1>
+      <div className="search">
+        <input
+          placeholder="Search For your desired film"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
 
-    return (
-        <div className='app'>
-            <h1>Cinematic Adventure</h1>
-            <div className='search'>
-                <input
-                    placeholder='Search For your desired film'
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                />
-                <img
-                    src={SearchIcon}
-                    alt="search"
-                    onClick={() => searchMovies(searchTerm)}
-                />
-            </div>
-
-            {movies?.length > 0
-                ?(
-                <div className='container'>
-                    {movies.map((movie) => (
-                        <MovieCard movie={movie}/>
-                    ))}
-                </div>
-                ) : (
-                    <div className='empty'>
-                        <h2>No Movies by that name were found!</h2>
-                    </div>
-                )}
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No Movies by that name were found!</h2>
+        </div>
+      )}
     </div>
-    );
-}
+  );
+};
 
 export default App;
--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,23 +1,29 @@
-import React from "react"
+import React from "react";
 
 const MovieCard = ({ movie }) => {
-    return(
-        <div className='movie'>
-            <div>
-                <p>{movie.Year}</p>
-            </div>
+  return (
+    <div className="movie">
+      <div>
+        <p>{movie.Year}</p>
+      </div>
 
-            <div>
-                <img src={movie.Poster != 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt={movie.Title}/>
-            </div>
+      <div>
+        <img
+          src={
+            movie.Poster !== "N/A"
+              ? movie.Poster
+              : "https://via.placeholder.com/400"
+          }
+          alt={movie.Title}
+        />
+      </div>
 
-            <div>
-                <span>{movie.Type}</span>
-                <h1>{movie.Title}</h1>
-            </div>
-        </div>
-    )
-}
-
+      <div>
+        <span>{movie.Type}</span>
+        <h1>{movie.Title}</h1>
+      </div>
+    </div>
+  );
+};
 
 export default MovieCard;
